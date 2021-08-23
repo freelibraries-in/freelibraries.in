8c317b015913688e119ab9bdd5017b3b6211d402
pragya-agrawal-x <pragya.agrwal@gmail.com>
2021-08-23 16:40:16 +0530

    Initial commit - Added index file

---
+++ resources/maindirectory.js
@@ -0,0 +1,27 @@
+freelibraries = [
+	{
+		"LIBRARY_NAME": "Share a Book India Association [HQ]",
+		"ADDRESS": "Kota, Rajasthan",
+		"MAP": "https://www.google.com/maps/place/Share+A+Book+India+Association/@25.1511951,75.8406709,17z/data=!3m1!4b1!4m5!3m4!1s0x396f85d9697f4617:0xe13a87c2f188b8d!8m2!3d25.1511951!4d75.8428596?hl=en"
+	},
+	{
+		"LIBRARY_NAME": "The Community Library Project - South Extension",
+		"ADDRESS": "South Extension, Gurudwara Rd, South Extension, Housing Society, Block C, South Extension I, New Delhi, Delhi",
+		"MAP": "https://www.google.com/maps/place/The+Community+Library+Project+-+South+Extension/@28.5700834,77.2207016,17z/data=!3m1!4b1!4m5!3m4!1s0x390ce39e73dc8867:0x5d734849f83574d1!8m2!3d28.5700908!4d77.2228914?hl=en"
+	},
+	{
+		"LIBRARY_NAME": "The Community Library Project - Khirki Extension",
+		"ADDRESS": "Khirki Extension, Khirki Extension, Block J, Khirki Extension, Malviya Nagar, New Delhi, Delhi",
+		"MAP": "https://www.google.com/maps/place/The+Community+Library+Project+-+Khirki+Extension/@28.5322147,77.2161248,17z/data=!3m1!4b1!4m5!3m4!1s0x390ce220d873c253:0x6f0205448b83764b!8m2!3d28.5322147!4d77.2183135?hl=en"
+	},
+	{
+		"LIBRARY_NAME": "The Community Library Project - Sikanderpur",
+		"ADDRESS": "Sikanderpur, DLF Phase 1, Sector 26, Gurugram, Haryana",
+		"MAP": "https://www.google.com/maps/place/The+Community+Library+Project+-+Sikanderpur/@28.4814276,77.0966054,17z/data=!3m1!4b1!4m5!3m4!1s0x390d1928cf23dbaf:0xb00ef2a36f9377b6!8m2!3d28.4814276!4d77.0987941?hl=en"
+	},
+	{
+		"LIBRARY_NAME": "Bansa Community Library & Resource Centre",
+		"ADDRESS": "Near Ram Janki Temple, Village Bansa, Block, Mallawan, Uttar Pradesh 241303",
+		"MAP": "https://www.google.com/maps/place/Bansa+Community+Library+%26+Resource+Centre/@27.0805224,80.2018418,17z/data=!3m1!4b1!4m5!3m4!1s0x399e8fc5a6f9ff03:0x4d57a40e7d717912!8m2!3d27.0805224!4d80.2040305?hl=en"
+	}
+]
